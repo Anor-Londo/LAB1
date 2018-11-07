2f138b3cb1d288b851aea6f35094d8608c8cb47a
Michael <Mishareznik@yandex.ru>
2018-11-07 19:57:51 +0300

    work!

--- main.go
+++ main.go
@@ -3,13 +3,21 @@ package main
 import (
 	"log"
 	"net/http"
+	"os"
 
 	"github.com/gorilla/mux"
 )
 
 func main() {
+	port := os.getenv("PORT")
+	var addr string
+	if port == "" {
+		addr = "localhost:8080"
+	} else {
+		addr = ":" + port
+	}
 	mux := mux.NewRouter()
 	mux.HandleFunc("/", helloHandle)
 	mux.HandleFunc("/{name}", sayHi)
-	log.Println(http.ListenAndServe("localhost:8080", mux))
+	log.Println(http.ListenAndServe(addr, mux))
 }
